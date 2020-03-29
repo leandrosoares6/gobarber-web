e2bccf300248e7ed03eb8adffd70164a67e1faf6
Leandro Neves <leandrosoares6@hotmail.com>
2020-03-29 16:35:24 -0300

    Added loading and error toasts in authentication

--- src/store/modules/user/reducer.js
+++ src/store/modules/user/reducer.js
@@ -6,7 +6,17 @@ const INITIAL_STATE = {
 
 export default function user(state = INITIAL_STATE, action) {
   switch (action.type) {
+    case '@auth/SIGN_IN_REQUEST':
+      return produce(state, draft => {
+        draft.profile = action.payload.user;
+      });
+
     case '@auth/SIGN_IN_SUCCESS':
+      return produce(state, draft => {
+        draft.profile = action.payload.user;
+      });
+
+    case '@auth/SIGN_IN_FAILURE':
       return produce(state, draft => {
         draft.profile = action.payload.user;
       });
